c7a8c3612948de06db00ef1e763d53dae30a0a96
amaninder <asingh15@mail.greenriver.edu>
2014-10-05 21:52:55 -0700

    Version 2 of Cards.c

--- cards.c
+++ cards.c
@@ -1,6 +1,6 @@
 //Amaninder Singh
 // IT 301
-// Version 1 
+// Version 2 
 // cards.c
 
 #include <stdio.h>
@@ -11,22 +11,38 @@ int main(void)
 {
 	
 	int val = 0;
+	int count = 0;
 	char card_name[3];
+	while (card_name[0] != 'X')
+	{
 	puts("Enter the card_name");
 	scanf("%2s", card_name);
-
-	if (card_name[0] == 'K')
-		{ val = 10; }
-	else if (card_name[0] == 'Q')
-		{	val = 11; }
-	else if (card_name[0] == 'J')
-		{	val = 10;}
-	else if (card_name[0] == 'A')
-		{	val = 11;}
-	else {
-		val = atoi(card_name);
-	}
+	switch(card_name[0] ){
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			continue;
+		default:
+			val = atoi(card_name);
+			if( (val< 1) || (val >10)){
+				puts("Please enter the value in the range \n");
+				continue;
+			}
+		}
+		if((val > 2) && (val < 7 ))
+			{ count++;  }
 	
+		else if  (val == 10){
+		count --;
+		}
+		printf("Current count: %i\n", count);
+	}
 
 	return 0;
 }
