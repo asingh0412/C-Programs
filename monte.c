6f83db9e1d938d25f196d52cf327b8b4b0a9880f
amaninder <asingh15@mail.greenriver.edu>
2014-10-07 11:28:59 -0700

    Version 2

--- monte.c
+++ monte.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	char *cards = "JQK";
+	char cards[] = "JQK";
 	char a_card = cards[2];
 	cards[2] = cards[1];
 	cards[1] = cards[0];
@@ -16,5 +16,5 @@ int main(void)
 	cards[1] = a_card;
 	puts(cards);
 	return 0;
-	
+
 }
